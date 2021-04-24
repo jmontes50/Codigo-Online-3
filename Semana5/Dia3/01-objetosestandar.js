b0e68af4d9c4edaeedb98759f3af28c4f37a9e93
jmontes50 <osmarmontes50@gmail.com>
2021-04-24 15:56:57 -0500

    update

---
+++ Semana5/Dia3/01-objetosestandar.js
@@ -0,0 +1,23 @@
+let auto = {
+  color:"azul",
+  marca:"Chevrolet",
+  anio:"2019"
+}
+
+console.log(auto.marca)
+//2do manera de acceder a propiedades, utilizando -> 
+//objeto["nombre_propiedad"]
+console.log(auto["color"])
+
+let arreglo = ["Agua","Limonada","Mojito","Jugo de Naranja","Pisco Sour"];
+
+console.log(arreglo.length)
+
+let banda = "Red Hot Chili Peppers";
+
+console.log(banda.length)
+
+//let mayorDeEdad = 18;
+let mayorDeEdad = new Number("18") //Demostrar que todo es un objeto
+
+console.log(mayorDeEdad + 20)
---
+++ Semana5/Dia3/02-metodosarreglos.js
@@ -0,0 +1,16 @@
+let bebidas = ["Inka Cola","Sprite","Frugos","Margarita"]
+
+console.log(bebidas)
+
+let nuevaBebida = prompt("Ingrese una nueva Bebida")
+
+//push agrega un elemento al final de un arreglo
+bebidas.push(nuevaBebida)
+
+console.log(bebidas)
+
+let frutas = ["naranja","piña","fresas"]
+
+frutas.pop()
+
+console.log(frutas)
