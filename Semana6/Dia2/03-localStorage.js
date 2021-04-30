22ac926de0e24fe70ec1a3e52f27b59a5e4b5fdd
jmontes50 <osmarmontes50@gmail.com>
2021-04-29 21:12:36 -0500

    update

--- Semana6/Dia2/03-localStorage.js
+++ Semana6/Dia2/03-localStorage.js
@@ -17,3 +17,24 @@ setTimeout(function(){
 
 //TODO LO QUE GUARDEMOS EN EL LOCALSTORAGE ES STRING
 localStorage.setItem("saludo", 8);
+
+let obj = {
+  fruta:"manzana",
+  colores:["verde","roja","mixta"],
+  tamanio:10,
+  sabor:"dulce y ácida"
+}
+
+//JSON.stringify convierte JS a texto
+let objAJSON = JSON.stringify(obj);
+console.log(objAJSON)
+
+//y recien lo guardamos en el LocalStorage
+localStorage.setItem("JSON",objAJSON);
+
+setTimeout(function(){
+  let JSONobtenido = localStorage.getItem("JSON")
+  let JSONdeVuelta = JSON.parse(JSONobtenido)
+  console.log(JSONdeVuelta)
+},2000)
+
