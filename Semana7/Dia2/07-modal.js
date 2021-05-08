fce94f96d583d49db85f15ca87fba7cc0970802f
jmontes50 <osmarmontes50@gmail.com>
2021-05-07 20:33:35 -0500

    update MOdal

---
+++ Semana7/Dia2/07-modal.js
@@ -0,0 +1,17 @@
+let btnModal = document.getElementById("btnModal");
+
+// let miModal = document.getElementById("miModal");
+//el Javascript que hemos importado de bootstrap me trae un opjeto llamado bootstrap
+let miModal = new bootstrap.Modal(document.getElementById("miModal"));
+ 
+console.log(miModal);
+//4.6 
+//5 !!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+btnModal.addEventListener("click", function(){
+  miModal.show()
+})
+
+setTimeout(function(){
+  miModal.show()
+},2000)
