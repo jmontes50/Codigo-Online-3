33bc8df63a73159635f28df74094b3d496a0d63e
jmontes50 <osmarmontes50@gmail.com>
2021-05-12 22:03:03 -0500

    update

---
+++ Semana8/Dia1/04-asincronia.js
@@ -0,0 +1,15 @@
+setTimeout(function(){
+  console.log("1. Horneamos la Torta")
+}, 4000);
+
+setTimeout(function(){
+  console.log("2. Ponemos el Chocolate al microondas")
+}, 3000);
+
+console.log("3. Preparo la torta con el chocolate y se decora")
+
+//1. Cómo funciona Javascript para hacer semejante salvajada, ya me bastaba con los tipos de datos y operaciones raras
+
+//2. Cómo solucionamos esto???????????? D: D: D: D: D:
+
+//3. Dónde está el libro de reclamaciones!!!!!????????
