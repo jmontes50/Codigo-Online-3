93897363e2b3285e7752dcdf868f4151490decfd
jmontes50 <osmarmontes50@gmail.com>
2021-05-13 19:29:26 -0500

    update

---
+++ Semana8/Dia2/06-callbacks.js
@@ -0,0 +1,23 @@
+let alumnosDB = ["Jhan","Erick","Máximo","Agustín","Bryan","Violeta","Veronica","Adriana"];
+
+let buscarAlumno = (nombre, callback) => {
+  //esta función va a devolver mediante un booleano si existe o no
+  setTimeout(() => {
+    //utilizamos setTimeout para que esto demore algo más
+    for(let i = 0; i < alumnosDB.length; i++){
+      if(alumnosDB[i] == nombre){
+        callback(true)
+        return; //cuando hacemos un return, se corta la ejecución
+      }
+    }
+    callback(false)
+  }, 3000)
+}
+buscarAlumno("Jhan", (existe) => {
+  if(existe === true){
+    console.log("El alumno existe")
+  }else{
+    console.log("No existe")
+  }
+})
+
