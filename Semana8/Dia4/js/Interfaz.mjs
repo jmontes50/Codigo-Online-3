312083ca4b76f18824bca538f9c21bfc58670944
jmontes50 <osmarmontes50@gmail.com>
2021-05-20 19:38:39 -0500

    update

--- Semana8/Dia4/js/Interfaz.mjs
+++ Semana8/Dia4/js/Interfaz.mjs
@@ -4,9 +4,27 @@ class Interface {
 
     let cardsContenido = '';
 
-    arregloProductos.forEach((item) => {
-      console.table(item)
+    arregloProductos.forEach(({id, nombre, precio, foto, descripcion}) => {
+      let cardProducto = `<div class="col-6 col-lg-3 mb-2">
+                            <div class="card">
+                              <img src="${foto}" class="card-img-top" alt="producto">
+                              <div class="card-body">
+                                <h4 class="card-title">
+                                  ${nombre}
+                                </h4>
+                                <p class="card-text">
+                                  ${descripcion}
+                                </p>
+                              </div>
+                              <div class="card-footer">
+                                <span class="fw-bold">Precio:</span> ${precio}
+                              </div>
+                            </div>
+                          </div>`;
+      cardsContenido = cardsContenido + cardProducto;                    
     })
+
+    contenido.innerHTML = cardsContenido;
   }
 }
 
