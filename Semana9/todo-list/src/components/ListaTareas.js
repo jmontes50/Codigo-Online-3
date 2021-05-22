7cf4be266b62e5c25f515265b5b7ee1726aa6663
jmontes50 <osmarmontes50@gmail.com>
2021-05-22 16:10:32 -0500

    updaTE

--- Semana9/todo-list/src/components/ListaTareas.js
+++ Semana9/todo-list/src/components/ListaTareas.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
 function ListaTareas({tareas}) {
-  console.log("misTareas", tareas)
+  // console.log("misTareas", tareas)
+  //renderizar Listas
   return (
     <div>
-      
+      {tareas.map((item, indice) => (
+        // este key tiene que ser único
+        <p key={indice}>
+          {item}
+          <button>X</button>
+        </p>
+        
+      ))}
     </div>
   )
 }
