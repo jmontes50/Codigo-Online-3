a3d1d324dc884262728f43dd7d2863d79eaa2cf8
jmontes50 <osmarmontes50@gmail.com>
2021-05-29 16:27:40 -0500

    update

--- Semana10/administrador/src/views/CrearCategoriaView.js
+++ Semana10/administrador/src/views/CrearCategoriaView.js
@@ -1,19 +1,30 @@
 import React, {useState} from 'react'
 import FormCategoria from "../components/FormCategoria"
 import {crearCategoria} from "../services/categoriaService"
+import Swal from "sweetalert2"
+import {useHistory} from "react-router-dom"
 
 function CrearCategoriaView() {
 
   const [value, setValue] = useState("")
+
+  const history = useHistory()
 
   const manejarSubmit = async (e) => {
     e.preventDefault()
     let nuevaCategoria = {
       nombre:value
     }
-
+    
     try {
       await crearCategoria({...nuevaCategoria})
+      await Swal.fire({
+        icon:"success",
+        title:"Categoria Creada",
+        showConfirmButton:false,
+        timer:2000
+      })
+      history.push('/')
     } catch (error) {
       console.log(error)
     }
