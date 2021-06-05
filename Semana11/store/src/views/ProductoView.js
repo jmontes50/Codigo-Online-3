49c0483a3010bb12ea2bcbf1014293594138939e
Jorge Montesinos <osmarmontes50@gmail.com>
2021-06-05 16:02:21 -0500

    update

--- Semana11/store/src/views/ProductoView.js
+++ Semana11/store/src/views/ProductoView.js
@@ -31,13 +31,36 @@ function ProductoView() {
       <div className="container">
         <div className="row">
           <div className="col-12 col-md-6">
-            <img src={producto.fotos[0]} alt={`foto de ${producto.nombre}`}/>
+            <img 
+              src={producto.fotos[0]} 
+              alt={`foto de ${producto.nombre}`}
+              className="img-fluid"
+            />
           </div>
           <div className="col-12 col-md-6">
-            <h5>Colores:</h5>
-            {producto.colores.map((item, i) => (
-              <div style={{width:'50px',height:'50px',backgroundColor:item}} key={i}/>
-            ))}
+            <h5 className="fw-bold">Colores:</h5>
+            <div className="d-flex mb-3">
+              {producto.colores.map((item, i) => (
+                <div style={{width:'50px',
+                            height:'50px',
+                            backgroundColor:item, 
+                            border:'1px solid gray',
+                            marginRight:'5px'}}
+                      key={i}/>
+              ))}
+            </div>
+            <h5 className="fw-bold">Descripción:</h5>
+            <p>{producto.descripcion}</p>
+            <hr/>
+            <div className="py-3 d-flex justify-content-between align-items-center">
+                <span className="fw-bold" style={{fontSize:'32px'}}>
+                  S/ {producto.precio}
+                </span>
+                <button className="btn btn-dark btn-lg">
+                  Añadir a Carrito
+                </button>
+            </div>
+            <hr/>
           </div>
         </div>
       </div>
