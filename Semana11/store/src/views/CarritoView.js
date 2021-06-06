db8d102ec9ef0ad2611f169a29e1d0634a1d67e9
Jorge Montesinos <osmarmontes50@gmail.com>
2021-06-05 19:01:03 -0500

    update

--- Semana11/store/src/views/CarritoView.js
+++ Semana11/store/src/views/CarritoView.js
@@ -4,10 +4,34 @@ import { CarritoContext } from "../context/carritoContext";
 function CarritoView() {
   let { carrito } = useContext(CarritoContext);
   return (
-    <div>
-      {carrito.map((prod, i) => (
-        <div key={i}>{prod.nombre}</div>
-      ))}
+    <div className="container">
+      <div className="my-4 text-center">
+        <h1 className="fw-bold">
+          <i className="fas fa-shopping-bag me-3"></i>
+          Carrito de Compras
+        </h1>
+      </div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Cantidad</th>
+            <th>Precio Unitario</th>
+            <th>Precio Total</th>
+          </tr>
+        </thead>
+        <tbody>
+        {carrito.map((prod, i) => (
+          <tr key={i}>
+            <td>{prod.nombre}</td>
+            <td>{prod.cantidad}</td>
+            <td>S/ {prod.precio}</td>
+            <td>S/ {prod.precio * prod.cantidad}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+      
     </div>
   );
 }
